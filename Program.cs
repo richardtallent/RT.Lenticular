6d0e047395047b29d78f618e3030ff32431a3f7d
Richard Tallent <richardtallent@Richard-MacMini.local>
2019-11-23 16:55:45 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.Primitives;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.Shapes;
+
+namespace RT.Lenticular {
+	class Program {
+		static void Main(string[] args) {
+			var c = new Calibration()
+			{
+				WidthInches = getIntArg(args, 0, 8),
+				HeightInches = getIntArg(args, 1, 8),
+				DPI = getIntArg(args, 2, 600),
+				LPI = getIntArg(args, 3, 60),
+				StepCentiLPI = getIntArg(args, 4, 100),
+				NumSteps = getIntArg(args, 5, 10)
+			};
+			Console.WriteLine($"{c.WidthInches} {c.HeightInches} {c.DPI} {c.LPI} {c.StepCentiLPI} {c.NumSteps}");
+			Console.WriteLine("Creating image");
+			using (var img = new Image<Rgba32>(null, c.WidthInches * c.DPI, c.HeightInches * c.DPI, Rgba32.White)) {
+				// Height in pixels of each block of lines
+				var lines = new List<int[]>();
+				var h = c.HeightInches * c.DPI / (c.NumSteps * 2 + 1);
+				var marginInches = 0.5f;
+				var font = SixLabors.Fonts.SystemFonts.CreateFont("Arial", h / 2, FontStyle.Regular);
+				//var fontBrush = new SolidBrush(Color.Black);
+				for (var i = -c.NumSteps; i <= c.NumSteps; i++) {
+					float thisCentiLPI = c.LPI * 100f + (i * c.StepCentiLPI);
+					var pixelsPerLine = (int)Math.Round(c.DPI / (thisCentiLPI / 100));
+					var p = new Pen(Color.Black, (float)pixelsPerLine);
+					var numLines = (c.WidthInches - marginInches) * c.DPI / 2 / pixelsPerLine;
+					var y = (i + c.NumSteps) * h;
+					Console.WriteLine($"Step: {pixelsPerLine} {numLines} {y}");
+					img.Mutate(ctx => ctx.DrawText(
+						$"{thisCentiLPI / 100}",
+						font,
+						Rgba32.Black,
+						new PointF(marginInches * c.DPI / 10, y + h / 5)
+					));
+					//img.DrawString($" Fooooo", font, fontBrush, new Point(c.DPI / 15, y + h / 10));
+					for (var j = 0; j < numLines; j++) {
+						var x = j * pixelsPerLine * 2 + (int)(marginInches * c.DPI);
+						var lh = (int)((float)h * .8);
+						//Console.WriteLine($"Line: {i} {j} {h} {x} {y} {y + lh}");
+						img.Mutate(ctx => ctx.DrawLines(
+							new GraphicsOptions(false),
+							Rgba32.Black,
+							pixelsPerLine,
+							new PointF(x, y),
+							new PointF(x, y + lh)
+						));
+					}
+				}
+				img.Save("test.png");
+			}
+		}
+
+		public static int getIntArg(string[] args, int place, int def) =>
+			(place > args.GetUpperBound(0)) ? def : Int32.Parse(args[place]);
+
+	}
+
+	class Calibration {
+		public int WidthInches { get; set; } = 8;
+		public int HeightInches { get; set; } = 8;
+		public int DPI { get; set; } = 600;
+		public int LPI { get; set; } = 60;
+		public int StepCentiLPI { get; set; } = 10;    // 100ths of line per inch to increment test
+		public int NumSteps { get; set; } = 4;          // On either side of the nominal side
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+Lenticular printing can create beautiful 3D or animated images, but it requires careful calibration to ensure that the interlacing of the frames of the image, as printed, match the measurements of the lenticular lens.
+
+I've just started experimenting with making these images, and this library comes out of my frustration at not being able to find good, \*_free_ software to calibrate my printer for the lenticular sheets I bought online (which are _theoretically_ 60lpi).
+
+After fiddling around in PhotoShop and deciding that was too time-consuming, I decided to make an app to generate a calibration image, which I could then print in PhotoShop and test.
+
+This is a command-line application, written in .NET Core. It should compiled and work on Windows, Linux, and MacOS (I use a Mac). The only non-framework dependency is ImageSharp, which seems to be fairly new, but it's far better than the old System.Drawing library, which is old and crusty and doesn't seem to work well outside of Windows.
+
+Use `dotnet run` to execute the application. Optional parameters can be used to tweak the output. To pass arguments, just pass each of these numbers (there are no "--parameter" or "-parameter" style prefixes):
+
+- Width, in inches (default: 8)
+- Height, in inches (default: 8),
+- DPI of your printer (default: 600)
+- LPI of your lenticular sheets (default: 60)
+- Step rate of the text, in 100ths of a a line, default 100 (i.e, jumps 1 LPI per test)
+- Number of test bars to generate on either side of the nominal LPI (default: 5, resulting in 11 bars)
+
+This will generate a PNG file with a bar of lines for each test, where the lines are spaced to create the test pattern. There are sites online explaining how to use these test sheets, so I won't belabor that, other than to mention that it's important that you _do not scale_ the graphic when you print it -- it must be printed at exactly the dimensions you selected. PhotoShop makes this easy, just select "Center," and before you print, use the Canvas Size to match the DPI there to the DPI used to generate the file.
+
+So far, this is only designed to create test patterns for stereographs (two images per lenticule). It could be expanded for other purposes, but I wanted to get this part right before refactoring it for more frames.
